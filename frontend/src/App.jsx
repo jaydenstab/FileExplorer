ea26a254b783520d60032acd429e2c3154932f98
Jayden Lam <jayjaylam57@gmail.com>
2025-11-16 15:17:57 -0500

    Add frontend: simplified UI with black background and title only

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1 className="title">file explorer</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
